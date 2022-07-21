99e8d2804a87a2e347513f3f4d9e109480e4141c
KALIANDRA DE FREITAS PEREIRA <kfreitaspereira1@gmail.com>
2022-07-21 16:59:45 -0400

    Adicionando pagina de Administrar categorias
    Tentativa de post, erro de backup , e mais frustrações

--- js/atacado/estoque/categorias.js
+++ js/atacado/estoque/categorias.js
@@ -1,5 +1,10 @@
 $(document).ready( function(){
     CarregarCategorias()
+
+    $('#btnINCLUSAO').click(function (){
+        localStorage.setItem('opercat','1');
+        window.location = 'admincategorias.html';
+    });
 });
 
  function CarregarCategorias() {
@@ -10,18 +15,28 @@ $(document).ready( function(){
 
         for (var i = 0; i < keys.length; i++) {
             var categoria = retorno[i];
-            var id = categoria.codigo;
-            var descricao = categoria.descricao;
-            var situacao = categoria.situacao;
+            var linha = '';
 
-            var linhaINI = "<tr>";
-            var colunaID = "<td>" + id + "</td>";
-            var colunaDESC = "<td>" + descricao + "</td>";
-            var colunaSIT = "<td>" + situacao + "</td>";
-            var linhaFIM = "</tr>";
-
-            var linha = linhaINI + colunaID + colunaDESC + colunaSIT + linhaFIM;
+            linha += "<tr>";
+            linha += "<td>" + categoria.codigo + "</td>";
+            linha += "<td>" + categoria.descricao + "</td>";
+            linha += "<td>" + categoria.situacao + "</td>";
+            linha +='<td><button id="btnALTERAR" onclick="AcionarAlteracao(' + categoria.codigo + ');">Alterar</button></td>';
+            linha += '<td><button id="btnEXCLUIR" onclick="AcionarExclusao(' + categoria.codigo + ');">Excluir</button></td>';
+            linha +="</tr>";
             $("#tblCategorias tbody").append(linha);
         }
     });
+}
+
+function AcionarAlteracao(id) {
+    localStorage.setItem('opercat','2');
+    localStorage.setItem('opercatid',id);
+    window.location = 'admincategorias.html';
+}
+
+function AcionarExclusao(id) {
+    localStorage.setItem('opercat','3');
+    localStorage.setItem('opercatid',id);
+    window.location = 'admincategorias.html';
 }
